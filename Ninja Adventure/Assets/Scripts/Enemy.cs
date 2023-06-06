02f8daf0eb088c34f7847d31d8d2668f91efc2b9
RamiroCeschini <forchiyceschi@gmail.com>
2023-06-06 01:26:39 -0300

    Movimiento básico de enemigo
    Haciendo uso del rigidbody2D, se define el movimiento del enemigo en seguimiento de un target

--- Ninja Adventure/Assets/Scripts/Enemy.cs
+++ Ninja Adventure/Assets/Scripts/Enemy.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float speed = 3.5f;
+
+    Rigidbody2D EnemyRigidbody2D;
+    private Vector2 enemyMovements;
+    public GameObject target;
+
+    public Vector2 EnemyMovements
     {
-        
+        get { return enemyMovements; }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        EnemyRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        
+
+        enemyMovements = new Vector2(target.transform.position.x, target.transform.position.y) - EnemyRigidbody2D.position;
+        enemyMovements.Normalize();
     }
+
+    public void FixedUpdate()
+    {
+        if (Vector2.Distance(new Vector2(target.transform.position.x, target.transform.position.y), EnemyRigidbody2D.position) < 5f)
+        {
+            EnemyRigidbody2D.MovePosition(EnemyRigidbody2D.position + speed * Time.deltaTime * enemyMovements);
+        }
+       
+    }
+
 }
