f7a0b89b77758f75023a24e0581b1e614ec42e69
TSC2002 <112729636+TSC2002@users.noreply.github.com>
2023-06-06 00:41:14 -0300

    Movimiento Del Jugador
    Mi tarea fue hacer el movimiento del jugador gracias al Rigidbody2D

--- Ninja Adventure/Assets/Scripts/PlayerMovement.cs
+++ Ninja Adventure/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 3.5f;
+
+    Rigidbody2D PlayerRigidbody2D;
+    private Vector2 playerMovements;
+
+    public Vector2 PlayerMovements
+    {
+        get { return playerMovements; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerMovements = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+
+        playerMovements.Normalize();
+    }
+
+    public void FixedUpdate()
+    {
+        PlayerRigidbody2D.MovePosition(PlayerRigidbody2D.position + speed * Time.deltaTime * playerMovements);
     }
 }
