4840991528df6a477bd8ff862ba49e96ccd1eebf
RamiroCeschini <forchiyceschi@gmail.com>
2023-08-05 14:59:06 -0300

    Interfaces declaradas

--- Ninja Defense/Assets/Scripts/Iinteractuable.cs
+++ Ninja Defense/Assets/Scripts/Iinteractuable.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Iinteractuable : MonoBehaviour
+public interface Iinteractuable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Accion();
 }
