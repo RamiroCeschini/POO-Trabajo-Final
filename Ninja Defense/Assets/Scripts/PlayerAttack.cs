1cd2dfefc11adc118909ae613294be1c0a89eabe
RamiroCeschini <forchiyceschi@gmail.com>
2023-08-06 18:15:29 -0300

    Weapon Rotation y attack animation

--- Ninja Defense/Assets/Scripts/PlayerAttack.cs
+++ Ninja Defense/Assets/Scripts/PlayerAttack.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator swordAnimator;
+    private bool isAttacking;
+
+    public bool IsAttacking
+    {
+        get { return isAttacking; }
+        private set { isAttacking = value; }
+    }
     void Start()
     {
-        
+        swordAnimator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LAMouse();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
     }
+
+    private void LAMouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+
+        Vector2 scale = transform.localScale;
+
+        if (direction.x > 0 )
+        {
+            scale.x = 1;
+        }
+
+        else if (direction.x < 0)
+        {
+            scale.x = -1;
+        }
+
+        transform.localScale = scale;
+    }
+
+    private void Attack()
+    {
+        swordAnimator.SetTrigger("Attack");
+        IsAttacking = true;
+    }
+
+    public void ResetIsAttacking()
+    {
+        IsAttacking = false;
+        Debug.Log("not attacking");
+    }
+
 }
